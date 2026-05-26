81a2055c22b0efba7beb53a07d33a90a76f0832b
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-05-26 10:18:40 +0530

    postgres mysql docker container command

---
+++ docker/db_server.sh
@@ -0,0 +1,29 @@
+# postgresql
+docker run -d \
+  --name my_postgres_alt \
+  -e POSTGRES_USER=appuser \
+  -e POSTGRES_PASSWORD=secret123 \
+  -e POSTGRES_DB=myappdb \
+  -p 5433:5432 \
+  -v pgdata_alt:/var/lib/postgresql/data \
+  postgres:16
+
+docker exec -it my_postgres_alt psql -U appuser -d myappdb
+
+# postgresql://appuser:secret123@localhost:5433/myappdb
+
+
+# mysql
+
+docker run -d \
+  --name my_mysql_alt \
+  -e MYSQL_ROOT_PASSWORD=rootpass \
+  -e MYSQL_DATABASE=myappdb \
+  -e MYSQL_USER=appuser \
+  -e MYSQL_PASSWORD=secret123 \ 
+  # important to set the password for the user
+  -p 3307:3306 \
+  -v mysqldata_alt:/var/lib/mysql \
+  mysql:8
+
+docker exec -it my_mysql_alt mysql -u appuser -p -D myappdb
