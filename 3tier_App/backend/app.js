be1a761bf3ab1de46fe91f118942219bbba7a63f
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-05-26 11:44:10 +0530

    docker project backend nodejs frontend nginx database mysql

---
+++ 3tier_App/backend/app.js
@@ -0,0 +1,63 @@
+import express from 'express';
+import pg from 'pg';
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import path from 'path';
+
+dotenv.config();
+const pool = mysql.createPool(process.env.DB);
+
+const app = express();
+const port = 4321;
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(cors({
+    origin: '*'// allow from any origin, you can specify your frontend URL here
+}));
+
+
+app.get('/api/dbtest', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/api/data', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM user where id=?', [1]);
+    res.json(result[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.post('/api/edit', async (req, res) => {
+  const { id, name, bio, github, leetcode, friend } = req.body;
+  try {
+    await pool.query('UPDATE user SET name = ?, bio = ?, github = ?, leetcode = ?, friend = ? WHERE id = ?', [name, bio, github, leetcode, friend, id]);
+    res.json({ message: 'User updated successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+app.get('/', (req, res) => {
+  // res.send('Hello from the backend!');  
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
--- docker/command.sh
+++ docker/command.sh
@@ -1,2 +1,13 @@
 docker pull <image_name>
 docker run <image_name>
+docker build -t <image_name> .
+docker run -p <host_port>:<container_port> <image_name>
+docker stop <container_name>
+docker rm <container_name>
+docker container prune -f
+
+
+docker network create appnet
+docker network connect appnet suspicious_gould
+docker network connect appnet app_mysql
+docker network inspect appnet
