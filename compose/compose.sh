9c703ec3b178b3a4e9a193fa0a1227a3a641000d
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-05-26 19:32:49 +0530

    docker compose started docker hub image pushed

---
+++ compose/compose.sh
@@ -0,0 +1,3 @@
+# compose is a tool for defining and running multi-container Docker applications. With Compose, you use a YAML file to configure your application's services. Then, with a single command, you create and start all the services from your configuration.
+docker -compose -f compose.yaml up -d
+docker -compose -f compose.yaml down
--- dockerHub/hub.sh
+++ dockerHub/hub.sh
@@ -1,3 +1,7 @@
+
 docker login -u <username> -p <password>
 docker pull <image_name>
-docker push <image_name>
+
+docker tag <username>/<image_name>:<tag> <username>/<image_name>:latest
+docker push <username>/<image_name>:latest
+docker logout
