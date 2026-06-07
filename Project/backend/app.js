421099150983e718168be9153615ed058c4719b2
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-06-07 15:54:06 +0530

    all files in the project are configured, app is running with compose.yaml

--- Project/backend/app.js
+++ Project/backend/app.js
@@ -3,7 +3,6 @@ import pg from 'pg';
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import path from 'path';
 
 dotenv.config();
 const pool = mysql.createPool(process.env.DB);
@@ -11,15 +10,16 @@ const pool = mysql.createPool(process.env.DB);
 const app = express();
 const port = 4321;
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 app.use(cors({
-    origin: "*" // allow from any origin, you can specify your frontend URL here
+    origin: "*"
 }));
 
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
 
 app.get('/api/dbtest', async (req, res) => {
   try {
@@ -52,11 +52,6 @@ app.post('/api/edit', async (req, res) => {
   }
 });
 
-
-app.get('/', (req, res) => {
-  // res.send('Hello from the backend!');  
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
